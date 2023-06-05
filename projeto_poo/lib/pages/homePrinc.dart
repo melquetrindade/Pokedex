59103497c5011752f98952a7e478c67b6987ca66
melquetrindade <melquetrindade654@gmail.com>
2023-06-05 14:14:32 -0300

    finalizando o mini-projeto da pokedex

--- projeto_poo/lib/pages/homePrinc.dart
+++ projeto_poo/lib/pages/homePrinc.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/pages/searchPoke.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
 class HomePrinc extends HookWidget {
@@ -10,81 +9,97 @@ class HomePrinc extends HookWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        width: double.infinity,
-        height: double.infinity,
-        decoration: BoxDecoration(
-            image: DecorationImage(
-                image: AssetImage('assets/imgs/capa1.jpg'), fit: BoxFit.cover)),
-        child: Padding(
-          padding: const EdgeInsets.only(top: 70),
-          child: Column(children: [
-            Image.asset(
-              'assets/imgs/logo2.png',
-              width: 300,
-            ),
-            Padding(
-              padding: const EdgeInsets.only(top: 60),
-              child: Center(
-                child: GestureDetector(
-                  onTap: () => onTap('/poke'),
-                  child: Container(
-                    child: Center(
-                      child: Text(
-                        'Entrar na Pokedex',
-                        style: TextStyle(color: Colors.white, fontSize: 30),
+      backgroundColor: Colors.red[800],
+      body: Padding(
+        padding: const EdgeInsets.only(top: 70),
+        child: Center(
+          child: ListView(
+            children: [
+              Container(
+                child: Column(
+                  children: [
+                    Image.asset(
+                      'assets/imgs/logo2.png',
+                      width: 300,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 40),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(width: 1, color: Colors.white),
+                          color: Colors.red.withOpacity(0.1),
+                          borderRadius: BorderRadius.circular(10)
+                        ),
+                        width: 320,
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            children: [
+                              TextField(
+                                decoration: InputDecoration(
+                                    labelText: "E-mail",
+                                    prefixIcon: Icon(Icons.mail),
+                                    border: OutlineInputBorder())),
+                              Container(
+                                height: 20,
+                              ),
+                              TextField(
+                                obscureText: true,
+                                decoration: InputDecoration(
+                                  labelText: "Senha",
+                                  prefixIcon: Icon(Icons.password),
+                                  border: OutlineInputBorder()),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(top: 15),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Padding(
+                                      padding: const EdgeInsets.only(left: 10),
+                                      child: Flexible(child: Text("Criar Conta", style: TextStyle(color: Colors.white))),
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.only(right: 10),
+                                      child: Flexible(child: Text("Esqueceu a senha?", style: TextStyle(color: Colors.white))),
+                                    )
+                                  ],
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(top: 25),
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                    //color: Colors.black.withOpacity(0.6),
+                                    border: Border.all(width: 1, color: Colors.white),
+                                    borderRadius: BorderRadius.circular(10)
+                                  ),
+                                  width: 300,
+                                  height: 45,
+                                  child: GestureDetector(
+                                    onTap: () => onTap('/poke'),
+                                    child: Container(
+                                      child: Center(
+                                        child: Text(
+                                          'Entrar',
+                                          style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
                       ),
                     ),
-                  ),
+                  ],
                 ),
-              ),
-            ),
-          ]),
+              )
+            ]),
+          ),
         ),
-      ),
     );
   }
 }
-
-/*
-        child: GestureDetector(
-          onTap: () => onTap('/poke'),
-          child: Container(
-            child: Center(
-              child: Text(
-                'Entrar na Pokedex',
-                style: TextStyle(color: Colors.black, fontSize: 20),
-              ),
-            ),
-          ),
-        ),*/
-/*
-TextField(
-                  decoration: InputDecoration(
-                      labelText: "E-mail",
-                      prefixIcon: Icon(Icons.mail),
-                      border: OutlineInputBorder())),
-                Container(
-                  height: 20,
-                ),
-                TextField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                      labelText: "Senha",
-                      prefixIcon: Icon(Icons.password),
-                      border: OutlineInputBorder()),
-                ),
-              ],
-              ),
-              
-              GestureDetector(
-                onTap: () => onTap('/poke'),
-                child: Container(
-                  child: Center(
-                    child: Text(
-                      'Entrar na Pokedex',
-                      style: TextStyle(color: Colors.black, fontSize: 20),
-                    ),
-                  ),
-                  ),
-                ),*/
